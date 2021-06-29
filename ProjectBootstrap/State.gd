4ba964788cce9048ccf55fd58e3c4e70695bb35c
Nick Pitrak <pitrak1@gmail.com>
2021-06-28 21:13:55 -0500

    fix printing

--- ProjectBootstrap/State.gd
+++ ProjectBootstrap/State.gd
@@ -4,16 +4,16 @@ var _network
 
 func _ready():
 	_network = get_node("/root/Network")
-	console.log("Entered state " + self.name)
+	print("Entered state " + self.name)
 	$UICanvasLayer/LoadingLabel.visible = false
 	$Camera2D.make_current()
 	
 func send_network_command(command_type, data):
-	console.log("Sending " + command_type + " command...")
+	print("Sending " + command_type + " command...")
 	_network.client_handle_outgoing_network_command(command_type, data)
 	$UICanvasLayer/LoadingLabel.visible = true
 	
 func on_receive_network_response(command_type, data):
-	console.log("Handling " + command_type + " response with status " + data['status'] + "...")
+	print("Handling " + command_type + " response with status " + data['status'] + "...")
 	self.call(command_type + "_response", data)
 	$UICanvasLayer/LoadingLabel.visible = false
